aaee25152f9af5a56b31f354e8aa5b011bd3424a
Zach Hanson <zachhanson94@gmail.com>
2021-12-15 05:12:13 -0800

    stuff

--- vulnerable-application/src/main/java/com/example/log4shell/LoginServlet.java
+++ vulnerable-application/src/main/java/com/example/log4shell/LoginServlet.java
@@ -1,39 +1,36 @@
 package com.example.log4shell;
 
 import java.io.*;
+import java.util.Enumeration;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-import com.sun.deploy.net.HttpRequest;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 
-@WebServlet(name = "loginServlet", value = "/login")
+@WebServlet(name = "loginServlet", value = "/pwnme")
 public class LoginServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-        String userName = req.getParameter("uname");
-        String password = req.getParameter("password");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
-        out.println("<html><body>");
-
-        if(userName.equals("admin") && password.equals("password")){
-            out.println("Welcome Back Admin");
+        out.println("<html><body><pre>");
+        Logger logger = LogManager.getLogger(com.example.log4shell.log4j.class);
+       Enumeration enumeration = req.getHeaderNames();
+        while (enumeration.hasMoreElements()) 
+        {
+             String header = (String) enumeration.nextElement();
+             out.println(header + ": " + req.getHeader(header) + "\n");
+                 logger.info(header + ": " + req.getHeader(header) + "\n" );
+                 logger.error(req.getHeader(header));
         }
-        else{
-
-            // vulnerable code
-            Logger logger = LogManager.getLogger(com.example.log4shell.log4j.class);
-            logger.error(userName);
-
-            out.println("<code> the password you entered was invalid, <u> we will log your information </u> </code>");
-        }
+        out.println("</pre>");
+        out.println("<code>did it work?</code>");
     }
 
     public void destroy() {
